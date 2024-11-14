b4a0aa589ef1e3f9b6e9122c70506f5607ea4d0d
Vitor Hugo Rodrigues <vitorhugorodrigues93@gmail.com>
2024-11-14 10:37:13 -0300

    first commit

---
+++ maui-in-azure/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace maui_in_azure
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ maui-in-azure/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace maui_in_azure
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
